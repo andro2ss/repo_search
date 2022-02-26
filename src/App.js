b0030834703c282dbf095f3b1909cc72ed2dff69
andro2ss <a.brzeski@hotmail.com>
2022-02-25 16:25:45 -0800

    added states and two views

--- src/App.js
+++ src/App.js
@@ -1,119 +1,48 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Container } from "@mui/material";
-import HideAppBar from "./components/common/AppBar";
+import HideAppBar from "./components/common/appBar/AppBar";
+import SearchPage from "./components/pages/searchPage/SearchPage";
+import DetailsPage from "./components/pages/detailsPage/DetailsPage";
+import { useState } from "react";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+  const [repository, setRepository] = useState({});
+  const [searchRepo, setSearchRepo] = useState("");
+  const [status, setStatus] = useState(0);
+  const [pageLocationHome, setPageLocationHome] = useState(true);
+
   return (
-    <Container fixed>
-      <HideAppBar />
-      <div>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
-        adipisci alias atque consequatur, corporis cupiditate dolor dolore
-        dolorem doloribus eaque explicabo inventore ipsum molestiae mollitia
-        neque nesciunt nostrum praesentium, quasi quia quis quo quos repellendus
-        sint vel, velit! Ab, deserunt ducimus eligendi molestias quis voluptate.
-        Aliquid architecto aspernatur atque aut culpa debitis delectus deleniti
-        doloremque doloribus dolorum expedita impedit itaque laboriosam laborum
-        magnam molestias mollitia nisi nostrum nulla optio perferendis, quae
-        quos, temporibus totam ullam unde voluptatem. Adipisci assumenda debitis
-        distinctio dolorum ducimus ea eius et facere illum impedit ipsa optio
-        placeat quas quasi reprehenderit repudiandae sapiente suscipit, vero
-        voluptate voluptatum. Alias asperiores, aspernatur cupiditate dicta ex
-        ipsa iusto libero maxime minus neque numquam perferendis quae, quo
-        reiciendis rem voluptatem voluptates voluptatum. Accusamus asperiores
-        assumenda consequatur dignissimos dolorum eaque et impedit labore
-        laboriosam minus nam natus nobis nulla odio pariatur perferendis
-        possimus quasi ratione sunt tempora tempore temporibus ullam unde
-        veniam, voluptatem. Ab aliquam assumenda dolor eum ipsa magni maxime
-        praesentium quisquam, recusandae repellendus similique tempora vitae
-        voluptatem. Aliquid autem beatae culpa cupiditate debitis est et
-        excepturi hic, ipsum, labore minus molestiae nemo non omnis praesentium
-        quasi qui quisquam quos rem repellat, repellendus sapiente sequi
-        temporibus ut voluptas. Ab eos illo itaque natus, nihil non perferendis
-        quas ut! A adipisci amet animi corporis eveniet iusto molestiae ratione
-        tenetur velit. Ad alias doloremque ex excepturi explicabo harum hic
-        incidunt ipsam labore laboriosam maiores mollitia nemo neque, nulla
-        omnis optio, porro provident quaerat quam quod rerum similique soluta
-        ullam unde voluptates. Adipisci aliquam architecto commodi consequatur
-        corporis dicta dolorem exercitationem facere fugiat iste itaque maxime
-        minima modi mollitia obcaecati, officiis quaerat quam quis quisquam quo
-        recusandae repellat reprehenderit, repudiandae similique sint sit
-        soluta. Est harum iusto nam nulla odio quis rem saepe sequi unde velit?
-        Doloribus harum illo ipsum! Adipisci asperiores atque autem consequatur
-        cumque deleniti, dolor dolores eligendi esse ex facere id illum impedit
-        incidunt ipsam, itaque iure labore maiores maxime molestiae nesciunt
-        officia officiis porro possimus quaerat quas ratione recusandae rem
-        rerum similique, sint tempore unde ut veritatis vero voluptas
-        voluptatem. Beatae debitis dolore ea eum iusto maiores, mollitia
-        necessitatibus obcaecati provident quam qui ratione soluta, sunt tempora
-        tenetur voluptate voluptatum. A amet atque autem deleniti dolor dolore
-        dolores dolorum eum in ipsum laudantium nostrum porro quaerat quam rem,
-        unde vel vero? Aliquid atque dignissimos eos eveniet magni maxime
-        officia optio voluptate? Accusamus cupiditate dolor eaque, esse illo nam
-        natus nobis voluptatem. Ab ad error id illo maxime, nobis officia porro
-        quia quisquam repellat sunt ullam veniam vitae voluptate voluptatum? Aut
-        consectetur corporis, delectus distinctio dolore doloribus ducimus ea
-        eaque iure laboriosam magnam maiores minima, pariatur quibusdam ratione
-        recusandae ut! Aliquid blanditiis ipsum itaque iure libero modi
-        molestiae quae, quod. Harum hic molestiae nulla soluta voluptatum.
-        Aperiam corporis cumque distinctio dolore, ex fugiat natus quidem
-        repellendus! Dicta dolor laudantium perspiciatis voluptates! Atque
-        beatae cumque eos illo incidunt libero officia quas recusandae, sit
-        vero. Delectus eum, illum laboriosam magni optio porro praesentium!
-        Aliquid blanditiis consectetur corporis est iste magni nihil quis soluta
-        tempora velit. Consequatur eaque, eos fugit iure maiores rerum tempore
-        voluptatibus. Ab eius esse est ex facere fugit rem reprehenderit saepe
-        sit totam. A architecto deserunt dicta ea eaque, enim eos, et ex fuga
-        labore maxime nam necessitatibus nihil non numquam quae qui sint ut
-        veritatis vitae? Blanditiis commodi distinctio, dolores ex
-        exercitationem, illo laudantium officia optio qui reprehenderit sint ut
-        voluptatum. Animi blanditiis explicabo id minima officiis reiciendis
-        repellendus? Adipisci alias architecto beatae cumque debitis delectus
-        deleniti dignissimos distinctio doloremque ducimus ex excepturi
-        exercitationem id incidunt inventore ipsa modi molestiae nisi,
-        perferendis possimus praesentium quae quaerat rerum totam ut vero
-        voluptas? Aliquid delectus dolorem doloremque doloribus, ea eligendi
-        enim exercitationem explicabo facere in ipsam iure maiores minus
-        perferendis quam quia repellat sapiente ullam vero voluptas. Aperiam
-        autem fugiat natus nisi obcaecati quia quidem sit! Ad, alias aliquid
-        aspernatur dolores, earum excepturi illo minima quos sed suscipit,
-        veritatis vero vitae voluptatem! Beatae culpa cupiditate harum iure
-        mollitia quaerat rerum sit voluptate! Aliquam aperiam assumenda autem
-        commodi corporis culpa cumque debitis delectus, dicta distinctio dolor
-        dolorem enim eveniet fugiat ipsam ipsum iusto libero maxime minus nihil
-        nobis numquam obcaecati perferendis, porro provident quam quasi quod
-        saepe sint sunt tempora totam vel veritatis! Aspernatur enim error esse
-        fuga magni, molestiae omnis quae ratione voluptas. Accusantium debitis
-        ducimus enim error facere hic iste, minus qui quisquam soluta! Ad
-        adipisci eligendi facere facilis fuga, nesciunt odit possimus. Aliquam
-        assumenda beatae, blanditiis consequatur cumque cupiditate eaque esse
-        explicabo id illum itaque minus nemo nulla officiis pariatur, qui quis
-        reiciendis soluta temporibus veniam. Aperiam deserunt dolor doloribus
-        dolorum eligendi eos fugiat rerum vel. Delectus dolorem excepturi modi
-        non optio provident quas recusandae tenetur unde ut! Ab atque dolores
-        eos fuga in iure iusto laudantium minus, necessitatibus, nemo, nesciunt
-        quisquam repudiandae totam? A aliquid aut blanditiis, consectetur
-        delectus deserunt dignissimos esse eveniet facere illo inventore nobis,
-        perferendis possimus quis sit? Aliquam aperiam assumenda autem
-        blanditiis commodi consectetur culpa cum cumque dignissimos doloremque
-        ea earum eos et eveniet explicabo illum ipsa ipsum laborum laudantium
-        magni maiores maxime modi molestias nihil non nulla, officiis omnis
-        perspiciatis placeat praesentium quaerat, qui quo sapiente soluta
-        suscipit temporibus velit. A ab accusantium asperiores dignissimos ea
-        earum et fuga laborum molestias nisi nostrum numquam obcaecati, pariatur
-        quod repudiandae sit totam ullam vero voluptate voluptatibus. A adipisci
-        animi autem beatae cum cupiditate debitis deleniti dignissimos dolorem
-        dolores earum eos exercitationem fuga fugit hic id illum incidunt iste
-        laborum laudantium molestiae nam natus nisi pariatur reprehenderit
-        rerum, soluta suscipit temporibus ullam voluptatum. Ad adipisci aliquam
-        aliquid amet consectetur corporis culpa debitis dolorum error esse, eum,
-        facilis fuga hic ipsa laboriosam maxime minima, minus molestiae nemo
-        nihil nobis odit omnis possimus quasi qui quia rem repellat repudiandae
-        rerum sed sequi soluta sunt suscipit tempore velit veniam voluptatibus!
-        Aliquam doloremque eos fuga in ipsam laborum laudantium, libero nostrum
-        numquam pariatur! A aliquam, atque culpa cumque dignissimos dolorum
-        earum eveniet id impedit itaque molestias nulla placeat praesentium quam
-        repellat, suscipit, tenetur? Autem iste maiores minus temporibus velit.
-      </div>
+    <Container fixed className="main__container">
+      <BrowserRouter>
+        <HideAppBar pageLocationHome={pageLocationHome} />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <SearchPage
+                repositories={repositories}
+                setRepositories={setRepositories}
+                setRepository={setRepository}
+                searchRepo={searchRepo}
+                setSearchRepo={setSearchRepo}
+                status={status}
+                setStatus={setStatus}
+                setPageLocationHome={setPageLocationHome}
+              />
+            }
+          />
+          <Route
+            path="/details"
+            element={
+              <DetailsPage
+                repository={repository}
+                setPageLocationHome={setPageLocationHome}
+              />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </Container>
   );
 }
